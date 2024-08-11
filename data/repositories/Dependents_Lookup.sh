0eceb97ef1549d18686935dfba32e29cce6a3511
JIMMY ZHAO <knightyzhao@gmail.com>
2024-08-11 00:14:18 -0400

    ok

--- data/repositories/Dependents_Lookup.sh
+++ data/repositories/Dependents_Lookup.sh
@@ -42,5 +42,5 @@ src search -json "context:global /import vlmeval|from vlmeval.* import / select:
 github-dependents-info --repo confident-ai/deepeval -p -j > "${path_repository}/DeepEval.json"
 github-dependents-info --repo huggingface/evaluate -p -j > "${path_repository}/Evaluate.json"
 github-dependents-info --repo evidentlyai/evidently -p -j > "${path_repository}/Evidently.json"
-github-dependents-info --repo embeddings-benchmark/mteb -p -j > "${path_repository}/Massive Text Embedding Benchmark.json"
+github-dependents-info --repo embeddings-benchmark/mteb -p -j > "${path_repository}/MTEB.json"
 github-dependents-info --repo explodinggradients/ragas -p -j > "${path_repository}/Ragas.json"
