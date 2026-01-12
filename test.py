e83710a25a0b6e07ca2b549b1909e283236886ca
zhimin-z <zhimin-z@users.noreply.github.com>
2026-01-12 10:17:39 -0500

    add

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_json("data/rq2_issues_annotated.jsonl", lines=True)
-df_sample_new = df[df['root_cause_label'].notna()]
-print(len(df_sample_new))
-df_sample_new.to_json("data/rq3_issue_sample_annotated.jsonl", orient="records", lines=True, force_ascii=False)
